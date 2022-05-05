290bfe527047dae0adbeaa6b5b10f0b64ed49da0
freerangerstudios <freeranger05@gmail.com>
2022-05-05 23:22:30 +0200

    Drop shadows + icons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,25 +2,25 @@
 
 <template>
     <div class="m-4 text-white">
-        <div class="bg-slate-700 p-4 pt-2 pb-2 rounded-lg">
+        <div class="bg-slate-700 p-4 pt-2 pb-2 rounded-lg drop-shadow-md">
             <h2 class="text-2xl font-semibold mb-2">Previous workouts</h2>
             <ul>
-                <li class="bg-slate-600 p-2 rounded-md mb-3 flex justify-between">
+                <li class="bg-slate-600 p-2 rounded-md mb-3 flex justify-between drop-shadow-md">
                     <div><p>Workout 1</p></div>
                     <div class="mr-2"><i class="fa-solid fa-angle-right" /></div>
                 </li>
 
-                <li class="bg-slate-600 p-2 rounded-md mb-3 flex justify-between">
+                <li class="bg-slate-600 p-2 rounded-md mb-3 flex justify-between drop-shadow-md">
                     <div><p>Workout 2</p></div>
                     <div class="mr-2"><i class="fa-solid fa-angle-right" /></div>
                 </li>
 
-                <li class="bg-slate-600 p-2 rounded-md mb-3 flex justify-between">
+                <li class="bg-slate-600 p-2 rounded-md mb-3 flex justify-between drop-shadow-md">
                     <div><p>Workout 3</p></div>
                     <div class="mr-2"><i class="fa-solid fa-angle-right" /></div>
                 </li>
 
-                <li class="bg-slate-600 p-2 rounded-md mb-3 flex justify-between">
+                <li class="bg-slate-600 p-2 rounded-md mb-3 flex justify-between drop-shadow-md">
                     <div><p>Workout 3</p></div>
                     <div class="mr-2"><i class="fa-solid fa-angle-right" /></div>
                 </li>
@@ -29,7 +29,7 @@
 
         <br />
 
-        <div class="bg-slate-700 p-4 pt-2 rounded-lg">
+        <div class="bg-slate-700 p-4 pt-2 rounded-lg drop-shadow-md">
             <h2 class="text-2xl font-semibold mb-2">Progress</h2>
             <p>No data available</p>
         </div>
--- src/views/Options.vue
+++ src/views/Options.vue
@@ -2,36 +2,36 @@
 
 <template>
     <div class="m-4 text-white">
-        <div class="bg-slate-700 p-4 pt-2 pb-2 rounded-lg">
+        <div class="bg-slate-700 p-4 pt-2 pb-2 rounded-lg drop-shadow-md">
             <h2 class="text-2xl font-semibold mb-2">Options</h2>
             <ul>
-                <li class="bg-slate-600 p-2 rounded-md mb-3 flex justify-between">
-                    <div><p>Account</p></div>
+                <li class="bg-slate-600 p-2 rounded-md mb-3 flex justify-between drop-shadow-md">
+                    <div><p><i class="fa-solid fa-user mr-3"/>Account</p></div>
                     <div class="mr-2"><i class="fa-solid fa-angle-right" /></div>
                 </li>
 
-                <li class="bg-slate-600 p-2 rounded-md mb-3 flex justify-between">
-                    <div><p>Appearance</p></div>
+                <li class="bg-slate-600 p-2 rounded-md mb-3 flex justify-between drop-shadow-md">
+                    <div><p><i class="fa-solid fa-palette mr-3"/>Appearance</p></div>
                     <div class="mr-2"><i class="fa-solid fa-angle-right" /></div>
                 </li>
 
-                <li class="bg-slate-600 p-2 rounded-md mb-3 flex justify-between">
-                    <div><p>Features</p></div>
+                <li class="bg-slate-600 p-2 rounded-md mb-3 flex justify-between drop-shadow-md">
+                    <div><p><i class="fa-solid fa-clipboard mr-3"/>Features</p></div>
                     <div class="mr-2"><i class="fa-solid fa-angle-right" /></div>
                 </li>
 
-                <li class="bg-slate-600 p-2 rounded-md mb-3 flex justify-between">
-                    <div><p>Notifications</p></div>
+                <li class="bg-slate-600 p-2 rounded-md mb-3 flex justify-between drop-shadow-md">
+                    <div><p><i class="fa-solid fa-bell mr-3"/>Notifications</p></div>
                     <div class="mr-2"><i class="fa-solid fa-angle-right" /></div>
                 </li>
 
-                <li class="bg-slate-600 p-2 rounded-md mb-3 flex justify-between">
-                    <div><p>Data</p></div>
+                <li class="bg-slate-600 p-2 rounded-md mb-3 flex justify-between drop-shadow-md">
+                    <div><p><i class="fa-solid fa-lock mr-3"/>Data</p></div>
                     <div class="mr-2"><i class="fa-solid fa-angle-right" /></div>
                 </li>
 
-                <li class="bg-slate-600 p-2 rounded-md mb-3 flex justify-between">
-                    <div><p>About</p></div>
+                <li class="bg-slate-600 p-2 rounded-md mb-3 flex justify-between drop-shadow-md">
+                    <div><p><i class="fa-solid fa-circle-info mr-3"/>About</p></div>
                     <div class="mr-2"><i class="fa-solid fa-angle-right" /></div>
                 </li>
             </ul>
