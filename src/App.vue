176b62d5192ec89a9e44725dd1ca904f721d8485
Freeranger <freeranger05@gmail.com>
2022-05-05 13:16:47 +0200

    Design started

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,41 @@
 </script>
 
 <template>
+    <div class="m-4">
+        <div class="bg-gray-100 p-4 pt-2 pb-2 rounded-lg">
+            <h2 class="text-2xl font-semibold mb-2">Previous workouts</h2>
+            <ul>
+                <li class="bg-gray-200 p-2 rounded-md mb-3 flex justify-between">
+                    <div><p>Workout 1</p></div>
+                    <div class="mr-2"><i class="fa-solid fa-angle-right"/></div>
+                </li>
+                
+                <li class="bg-gray-200 p-2 rounded-md mb-3 flex justify-between">
+                    <div><p>Workout 2</p></div>
+                    <div class="mr-2"><i class="fa-solid fa-angle-right"/></div>
+                </li>
+
+                <li class="bg-gray-200 p-2 rounded-md mb-3 flex justify-between">
+                    <div><p>Workout 3</p></div>
+                    <div class="mr-2"><i class="fa-solid fa-angle-right"/></div>
+                </li>
+            </ul>
+        </div>
+
+        <br>
+        <div class="bg-gray-100 p-4 pt-2 rounded-lg">
+            <h2 class="text-2xl font-semibold mb-2">Progress</h2>
+            <p>No data</p>
+        </div>
+    </div>
+
     <Navbar/>
 </template>
 
 <style>
-
+    #app{
+        padding: 0;
+        margin: 0;
+        font-family: 'Poppins';
+    }
 </style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,9 +4,10 @@
 
 <template>
     <div class="navbar fixed bottom-0 left-0 w-full bg-gray-100 rounded-t-2xl">
-        <ul class="text-center h-full flex flex-grow justify-center flex-row items-center text-2xl text-zinc-800">
+        <ul class="text-center h-full flex flex-grow justify-center flex-row items-center text-xl text-zinc-800">
             <li class="w-full"><a href="#"><i class="fa-solid fa-dumbbell"/></a></li>
-            <li class="w-full"><a href="#"><i class="fa-solid fa-house"/></a></li>
+            <li class="w-full"><a href="#"><i class="fa-solid fa-clipboard"/></a></li>
+            <li class="w-full text-2xl"><a href="#"><i class="fa-solid fa-house"/></a></li>
             <li class="w-full"><a href="#"><i class="fa-solid fa-rocket"/></a></li>
             <li class="w-full"><a href="#"><i class="fa-solid fa-cog"/></a></li>
         </ul>
